f024394febc9c2b40e977b4255ad324a50e58200
Erik <ermanfantasy@gmail.com>
2022-11-25 00:23:45 +0400

    Header : add and sort actions for cards

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,3 +1,28 @@
-export default function Header() {
-  return <div>Header</div>;
+import { memo } from "react";
+// redux
+import { useDispatch } from "react-redux";
+import { add, sort } from "providers/redux/slices";
+// components
+import { Box, Grid, Button } from "@mui/material";
+
+function Header() {
+  const dispatch = useDispatch();
+
+  const addNewCard = () => dispatch(add());
+  const sortCards = () => dispatch(sort());
+
+  return (
+    <Box sx={{ p: 1 }} component="header">
+      <Grid container spacing={2}>
+        <Grid item>
+          <Button onClick={addNewCard}>Add new card</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={sortCards}>Sort cards</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
+
+export default memo(Header);
